d331f8fe129a4fd313aaa718231107ebf861ef29
satty3104 <satty3104@gmail.com>
2017-01-31 00:38:33 +0900

    Add Bootstrap class

---
+++ src/main/java/pacman/app/Bootstrap.java
@@ -0,0 +1,16 @@
+package pacman.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public interface Bootstrap {
+
+  static void main(String[] args) {
+    SpringApplication.run(Bootstrap.class, args);
+  }
+}
